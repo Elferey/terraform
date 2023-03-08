9d26a10147edc58357f4e8169205f37f0bdc29ad
Elferey <elf.yanov@gmail.com>
2023-03-08 23:12:47 +0300

    add terraform files

--- template/config.tf
+++ template/config.tf
@@ -10,4 +10,6 @@ terraform {
 provider "yandex" {
   zone = "default-ru-central1-b"
   token = "y0_AgAAAABZGe1BAATuwQAAAADeCfMBYvcXU_fSRgCQ3AHpBuENy16kaTk"
+  cloud_id  = "enpi7gartbkjdm67rrgr"
+  folder_id = "b1gl0bm0dbk01pl1io4u"
 }
