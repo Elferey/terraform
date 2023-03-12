d458314ced721afe630e1d63a0260f6e40bc534a
Elferey <elf.yanov@gmail.com>
2023-03-12 16:44:28 +0300

    add grafana

---
+++ grafana/main.tf
@@ -0,0 +1,75 @@
+
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "build"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80d7fnvf399b1c207j"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9b1hsqivfmdc1ncavj9"
+    nat       = true
+  }
+  
+    metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/yandex_key_ssh.pub")}"
+    user-data = "${file("./data/build_mvn")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "deploy"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80d7fnvf399b1c207j"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9b1hsqivfmdc1ncavj9"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/yandex_key_ssh.pub")}"
+    user-data = "${file("./data/tomcat")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-3" {
+  name = "grafana"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80d7fnvf399b1c207j"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9b1hsqivfmdc1ncavj9"
+    nat       = true
+  }
+  
+    metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/yandex_key_ssh.pub")}"
+  }
+}
